a743207b068a649e81c9a39d8e04cd9a9c12cc46
romamik <roman@romamik.com>
2021-12-11 01:59:18 +0300

    day8 iteration. all tests except last pass.

--- src/day8.rs
+++ src/day8.rs
@@ -1,139 +1,291 @@
-use std::fs;
-use std::collections::HashSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fmt;
+use std::fs;
 
-type Digit = HashSet<u8>;
-type Digits = [Digit; 10];
-type InputLine = [Vec<Digit>; 2];
+const ALL_SEGMENTS: [char; 7] = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+/*
+Segment - segment of 7-segment display, named "a" to "g".
+Signal - set of turned on segments - for example "cf", or "acdeg"
+Mapping - mapping of segments to segments. Mapping can be represented as string of length 0 to 7. For example "cbd" - means that "a" is mapped to "c", "b" to "b" and "c" to
+
+We have
+    valid_signals: digits 0 to 9 rendered on 7-segment display.
+    input_signals: set of Signals that should be mapped using some Mapping so that each becomes one of the valid_signals
+    test_signals: set of Signals we should map using valid mapping
+
+fn possible_valid_signals(input_signal, mapping)
+    function to find to which of valid_signals input_signal can be mapped using mapping.
+    if mapping is complete, i.e. defines mapping for every segment - it's obvious: just apply mapping and find matches in valid_signals
+    if mapping is incomplete, we should map only segments that have defined mapping and then find valid_signals that have mapped segments turned on
+    One more thing to consider is that input_signal can possibly be mapped to valid_signal if they have the same length
+
+fn find_mapping (solve pt2)
+    function to find mapping that makes all input_signals to be mapped to one of valid_signals
+    we just test all possible mappings to see if they map each input_signal to exactly one valid_signal
+    but we do not generate all possible complete mappings, we start with shorter incomplete mappings and produce more complete mappings only for mappings that possibly can map input_signals to valid_signals
+
+    THIS DOES NOT WORK: After implementing above algorithm all test pass except the last one. 
+    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+    result is:
+        aefg => 4
+        abcfg => 2
+        fg => 1
+        abceg => 2
+
+    Looks like we should check final mapping
+*/
+
+type InputLine = [Vec<Signal>; 2];
 type Input = Vec<InputLine>;
 
-fn solve_pt1(digits: &Digits, input: &Input) -> i32 {
-    
-    let mut digits_by_len: HashMap<usize, Vec<&Digit>> = HashMap::new();
-    for digit in digits {
-        let len = digit.len();
-        match digits_by_len.get_mut(&len) {
-            Some(v) => v.push(&digit),
-            None => { digits_by_len.insert(len, vec![&digit]); }
+type Segment = char;
+
+#[derive(Clone)]
+struct Signal(String, HashSet<Segment>);
+
+impl Signal {
+    fn new(s: &str) -> Signal {
+        let hash_set: HashSet<Segment> = s.chars().collect();
+        let mut vec: Vec<Segment> = hash_set.iter().cloned().collect();
+        vec.sort();
+        let name: String = vec.iter().collect();
+        Signal(name, hash_set)
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn is_segment_on(&self, segment: Segment) -> bool {
+        self.1.get(&segment).is_some()
+    }
+
+    fn has_all_segments_on(&self, signal: &Signal) -> bool {
+        for segment in signal.1.iter() {
+            if !self.is_segment_on(*segment) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn map(&self, mapping: &HashMap<Segment, Segment>) -> Signal {
+        let mut mapped = String::new();
+        for segment in self.1.iter() {
+            if let Some(mapped_segment) = mapping.get(&segment) {
+                mapped.push(*mapped_segment);
+            }
+        }
+        Signal::new(&mapped)
+    }
+}
+
+impl fmt::Debug for Signal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+struct Mapping {
+    name: String,
+    map: HashMap<Segment, Segment>,
+    map_back: HashMap<Segment, Segment>,
+}
+
+impl Mapping {
+    fn new() -> Mapping {
+        Mapping {
+            name: String::new(),
+            map: HashMap::new(),
+            map_back: HashMap::new(),
         }
     }
 
-    let single_digits = digits_by_len.iter().filter_map(|(&_len, digits)| {
-        if digits.len() == 1 {
-            Some(digits[0])
-        }
-        else {
-            None
-        }
-    }).collect::<Vec<_>>();
-    
-    //println!("{:?}", digits_by_len);
-    //println!("{:?}", single_digits);
+    fn len(&self) -> usize {
+        self.name.len()
+    }
 
+    fn has_segment_mapping(&self, to: Segment) -> bool {
+        self.map_back.get(&to).is_some()
+    }
+
+    fn add_segment_mapping(&mut self, to: Segment) {
+        if self.has_segment_mapping(to) {
+            panic!()
+        }
+        let from: char = ALL_SEGMENTS[self.len()];
+        self.map.insert(from, to);
+        self.map_back.insert(to, from);
+        self.name.push(to);
+        //println!("add {} {:?}", to, self);
+    }
+
+    fn remove_last_segment_mapping(&mut self) {
+        let to = self.name.pop().unwrap();
+        let from = *self.map_back.get(&to).unwrap();
+        self.map.remove(&from);
+        self.map_back.remove(&to);
+        //println!("remove {} {:?}", to, self);
+    }
+}
+
+impl fmt::Debug for Mapping {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} map: {:?} map_back: {:?}",
+            self.name, self.map, self.map_back
+        )
+    }
+}
+
+fn solve_pt1(valid_signals: &Vec<Signal>, input: &Input) -> i32 {
     let mut count = 0;
-
-    for line in input {
-        for digit in line[1].iter() {
-            if single_digits.iter().position(|d| d.len() == digit.len()).is_some() {
+    for input_line in input.iter() {
+        for test_signal in input_line[1].iter() {
+            let possible_valid_signal_count = valid_signals
+                .iter()
+                .filter(|valid_signal| valid_signal.len() == test_signal.len())
+                .count();
+            if possible_valid_signal_count == 1 {
                 count += 1;
-                //println!("{:?}", digit);
             }
         }
     }
-
     count
 }
 
-fn solve_pt2_impl(digits: &Digits, input_line: &InputLine) -> i32 {
-
-    let mut possible_mappings = HashMap::new();
-    for i in 0..10 {
-        possible_mappings.insert(i, Vec::from_iter(0..10));
-    }
-
-    for input_digit in input_line[0] {
-        for digit in digits {
-            if digit_can_be_mapped_to_input_digit {
-                
+fn find_possible_valid_signals<'a>(
+    valid_signals: &'a Vec<Signal>,
+    signal: &'a Signal,
+    mapping: &'a Mapping,
+) -> Vec<&'a Signal> {
+    let mut possible_valid_signals = Vec::new();
+    for valid_signal in valid_signals {
+        if valid_signal.len() == signal.len() {
+            let mapped = signal.map(&mapping.map);
+            if valid_signal.has_all_segments_on(&mapped) {
+                let mapped_back = valid_signal.map(&mapping.map_back);
+                if signal.has_all_segments_on(&mapped_back) {
+                    possible_valid_signals.push(valid_signal);
+                }
             }
         }
     }
-
-    0
+    possible_valid_signals
 }
 
-fn solve_pt2(digits: &Digits, input: &Input) -> i32 {
+fn solve_pt2_line(valid_signals: &Vec<Signal>, input_line: &InputLine) -> i32 {
+    fn recursive_search(
+        valid_signals: &Vec<Signal>,
+        mapping: &mut Mapping,
+        input_signals: &Vec<Signal>,
+    ) -> bool {
+        let mut certain_count = 0;
+        for input_signal in input_signals {
+            let possible_count = find_possible_valid_signals(valid_signals, input_signal, mapping)
+                .iter()
+                .count();
+            match possible_count {
+                0 => return false, //mapping is impossible
+                1 => certain_count += 1,
+                _ => (),
+            }
+        }
+        if certain_count == input_signals.len() {
+            return true;
+        }
 
-    let mut sum = 0;
-    for line in input {
-        sum += solve_pt2_impl(digits, line)
+        for segment in ALL_SEGMENTS.iter().cloned() {
+            if !mapping.has_segment_mapping(segment) {
+                mapping.add_segment_mapping(segment);
+                if recursive_search(valid_signals, mapping, input_signals) {
+                    return true;
+                }
+                mapping.remove_last_segment_mapping();
+            }
+        }
+
+        false
     }
-    sum
+
+    let mut mapping = Mapping::new();
+    let mut combined_input = input_line[0].clone();
+    combined_input.extend(input_line[1].clone());
+    recursive_search(valid_signals, &mut mapping, &combined_input);
+    let mut result = 0;
+    println!("{:?}", mapping);
+    for test_signal in &input_line[1] {
+        let mapped = find_possible_valid_signals(valid_signals, test_signal, &mapping);
+        if mapped.len() != 1 {
+            panic!();
+        }
+        let mapped = mapped[0];
+        let mapped = valid_signals.iter().position(|s| s.0 == mapped.0).unwrap();
+        result *= 10;
+        result += mapped;
+        println!("{:?} => {:?}", test_signal, mapped);
+    }
+
+    result as i32
+}
+
+fn solve_pt2(valid_signals: &Vec<Signal>, input: &Input) {
+    for input_line in input.iter() {
+        let line_result = solve_pt2_line(valid_signals, input_line);
+        println!("{}", line_result);
+    }
 }
 
 pub fn main() {
+    let valid_signals: Vec<Signal> = [
+        "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg",
+    ]
+    .iter()
+    .map(|s| Signal::new(s))
+    .collect();
 
-    let digits = get_digits();
-    let test_input = parse_input("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce");
-
+    let test_input = "
+        be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    ";
+    let test_input = "
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    ";
+    let test_input = parse_input(test_input);
     let day8_input = parse_input(&fs::read_to_string("input/day8.txt").unwrap());
-
-    println!("test pt1 {}", solve_pt1(&digits, &test_input));
-    println!("day8 pt1 {}", solve_pt1(&digits, &day8_input));
-    println!("test pt2 {}", solve_pt2(&digits, &test_input));
-}
-
-fn get_digits() -> Digits {
-//   0:      1:      2:      3:      4:
-//  aaaa    ....    aaaa    aaaa    ....
-// b    c  .    c  .    c  .    c  b    c
-// b    c  .    c  .    c  .    c  b    c
-//  ....    ....    dddd    dddd    dddd
-// e    f  .    f  e    .  .    f  .    f
-// e    f  .    f  e    .  .    f  .    f
-//  gggg    ....    gggg    gggg    ....
-
-//   5:      6:      7:      8:      9:
-//  aaaa    aaaa    aaaa    aaaa    aaaa
-// b    .  b    .  .    c  b    c  b    c
-// b    .  b    .  .    c  b    c  b    c
-//  dddd    dddd    ....    dddd    dddd
-// .    f  e    f  .    f  e    f  .    f
-// .    f  e    f  .    f  e    f  .    f
-//  gggg    gggg    ....    gggg    gggg
-    [
-        "abefg", //0
-        "cf", // 1
-        "acdeg", // 2
-        "acdfg", // 3
-        "bcdf", // 4
-        "abdfg", // 5
-        "abdefg", // 6
-        "acf", // 7
-        "abcdefg", // 8
-        "abcdfg", // 9
-    ].map(|s| HashSet::from_iter(s.as_bytes().iter().cloned()))
+    println!("test pt1 {:?}", solve_pt1(&valid_signals, &test_input));
+    println!("day8 pt1 {:?}", solve_pt1(&valid_signals, &day8_input));
+    solve_pt2(&valid_signals, &test_input);
 }
 
 fn parse_input(s: &str) -> Input {
     s.split("\n")
-    .map(|s| 
+        .map(|s| s.trim())
+        .filter(|s| s.len() > 0)
+        .map(|s| parse_input_line(s))
+        .collect::<Vec<_>>()
+}
+
+fn parse_input_line(s: &str) -> InputLine {
+    InputLine::try_from(
         s.split("|")
-        .map(|s| 
-            s.split_whitespace()
-            .map(|s| HashSet::<u8>::from_iter(s.as_bytes().iter().cloned()))
-            .collect::<Vec<_>>()
-        ).collect::<Vec<_>>()
-        .try_into().unwrap()
+            .map(|s| {
+                s.split_whitespace()
+                    .map(|s| Signal::new(s))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>(),
     )
-    .collect::<Vec<_>>()
+    .unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
         day6::main();
         day7::main();
         day9::main();
+        day10::main();
     }
-    //day8::main();
-    day10::main();
+    day8::main();
 }
