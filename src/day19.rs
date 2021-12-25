f2678418527369e7afbcc699ad69963dd73bb273
romamik <roman@romamik.com>
2021-12-26 02:23:48 +0300

    day19 progress: add translations to matrices

--- src/day19.rs
+++ src/day19.rs
@@ -1,8 +1,8 @@
 /*
 Matrix and vector layout
-[[0, 1, 2],       [0,
- [2, 3, 4],        1,
- [5, 6, 7]]        2]
+[[00, 01, 02, 03],       [0,
+ [10, 11, 12, 13],        1,
+ [20, 21, 22, 23]]        2]
 
 X - face right, Y - face up, Z - from viewer
 
@@ -33,8 +33,8 @@ Z
 */
 
 use std::{
-    fmt,
     collections::HashMap,
+    fmt,
     ops::{Index, IndexMut},
 };
 
@@ -79,18 +79,18 @@ impl IndexMut<Axis> for Vector {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-struct Matrix([[i32; 3]; 3]);
+struct Matrix([[i32; 4]; 3]);
 
 impl Matrix {
     fn identity() -> Matrix {
-        Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
     }
 
     fn rotate90cw(axis: Axis) -> Matrix {
         match axis {
-            Axis::X => Matrix([[1, 0, 0], [0, 0, 1], [0, -1, 0]]),
-            Axis::Y => Matrix([[0, 0, -1], [0, 1, 0], [1, 0, 0]]),
-            Axis::Z => Matrix([[0, 1, 0], [-1, 0, 0], [0, 0, 1]]),
+            Axis::X => Matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, 0]]),
+            Axis::Y => Matrix([[0, 0, -1, 0], [0, 1, 0, 0], [1, 0, 0, 0]]),
+            Axis::Z => Matrix([[0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 1, 0]]),
         }
     }
 
@@ -100,6 +100,14 @@ impl Matrix {
             m = m.mul(&Self::rotate90cw(axis));
         }
         m
+    }
+
+    fn translate(v: &Vector) -> Matrix {
+        Matrix([
+            [1, 0, 0, v[Axis::X]],
+            [0, 1, 0, v[Axis::Y]],
+            [0, 0, 1, v[Axis::Z]],
+        ])
     }
 
     fn all_orientations() -> Vec<Matrix> {
@@ -124,30 +132,42 @@ impl Matrix {
     fn mul(&self, right: &Matrix) -> Matrix {
         let b = &right.0;
 
-        let v0 = self.apply(&Vector([b[0][0], b[1][0], b[2][0]])).0;
-        let v1 = self.apply(&Vector([b[0][1], b[1][1], b[2][1]])).0;
-        let v2 = self.apply(&Vector([b[0][2], b[1][2], b[2][2]])).0;
+        let v0 = self.apply4(b[0][0], b[1][0], b[2][0], 0);
+        let v1 = self.apply4(b[0][1], b[1][1], b[2][1], 0);
+        let v2 = self.apply4(b[0][2], b[1][2], b[2][2], 0);
+        let v3 = self.apply4(b[0][3], b[1][3], b[2][3], 1);
+
+        assert_eq!([v0[3], v1[3], v2[3], v3[3]], [0, 0, 0, 1]);
 
         Matrix([
-            [v0[0], v1[0], v2[0]],
-            [v0[1], v1[1], v2[1]],
-            [v0[2], v1[2], v2[2]],
+            [v0[0], v1[0], v2[0], v3[0]],
+            [v0[1], v1[1], v2[1], v3[1]],
+            [v0[2], v1[2], v2[2], v3[2]],
         ])
     }
 
     fn apply(&self, v: &Vector) -> Vector {
-        let m = &self.0;
         let v = &v.0;
-        Vector([
-            m[0][0] * v[0] + m[0][1] * v[1] + m[0][2] * v[2],
-            m[1][0] * v[0] + m[1][1] * v[1] + m[1][2] * v[2],
-            m[2][0] * v[0] + m[2][1] * v[1] + m[2][2] * v[2],
-        ])
+        let r = self.apply4(v[0], v[1], v[2], 1);
+        Vector([r[0], r[1], r[2]])
+    }
+
+    fn apply4(&self, v0: i32, v1: i32, v2: i32, v3: i32) -> [i32; 4] {
+        let m = &self.0;
+        let m0 = &m[0];
+        let m1 = &m[1];
+        let m2 = &m[2];
+
+        [
+            m0[0] * v0 + m0[1] * v1 + m0[2] * v2 + m0[3] * v3,
+            m1[0] * v0 + m1[1] * v1 + m1[2] * v2 + m1[3] * v3,
+            m2[0] * v0 + m2[1] * v1 + m2[2] * v2 + m2[3] * v3,
+            v3,
+        ]
     }
 }
 
 fn try_align(scanner0: &Vec<Vector>, scanner1: &Vec<Vector>, min_pair: usize) -> Option<Matrix> {
-
     None
 }
 
@@ -155,6 +175,7 @@ pub fn main() {
     let input = parse_input(&std::fs::read_to_string("input/day19.txt").unwrap());
     test_matrices();
     test_all_orientations();
+    test_matrix_translate();
     test0(input.get("test0").unwrap());
     test1(input.get("test1").unwrap());
 
@@ -162,33 +183,52 @@ pub fn main() {
 }
 
 fn test1(test1_input: &Input) {
-    
-    let scanners = (0..=4).map(|n| test1_input.get(&format!("scanner {}", n)).unwrap()).collect::<Vec<_>>();
+    let scanners = (0..=4)
+        .map(|n| test1_input.get(&format!("scanner {}", n)).unwrap())
+        .collect::<Vec<_>>();
 
     // try align scanners 0 and 1
     let align_result = try_align(scanners[0], scanners[1], MIN_PAIR).unwrap();
-    let transformed_1 = scanners[1].iter().map(|v| align_result.apply(v)).collect::<Vec<_>>();
-    assert_eq!(try_align(scanners[0], &transformed_1, MIN_PAIR), Some(Matrix::identity()));
-    
+    let transformed_1 = scanners[1]
+        .iter()
+        .map(|v| align_result.apply(v))
+        .collect::<Vec<_>>();
+    assert_eq!(
+        try_align(scanners[0], &transformed_1, MIN_PAIR),
+        Some(Matrix::identity())
+    );
 }
 
 fn test0(test0_input: &Input) {
-
     // all given scanners are same but in different orientations
     let first = test0_input.get("scanner 0-0").unwrap();
     for (_, vecs) in test0_input.iter() {
-        let found = Matrix::all_orientations().iter().any(|m| 
-            vecs.iter()
-            .zip(first.iter())
-            .all(|(v, first_v)| {
+        let found = Matrix::all_orientations().iter().any(|m| {
+            vecs.iter().zip(first.iter()).all(|(v, first_v)| {
                 let transformed = m.apply(v);
                 transformed == *first_v
-                
             })
-        );
+        });
         assert!(found);
     }
+}
 
+fn test_matrix_translate() {
+    let v = Vector([1, 2, 3]);
+    assert_eq!(
+        Matrix::translate(&Vector([1, 1, 1])).apply(&v),
+        Vector([2, 3, 4])
+    );
+
+    let m = Matrix::translate(&Vector([1, 2, 3]));
+    let m = m.mul(&Matrix::translate(&Vector([-1, -2, -3])));
+    assert_eq!(m, Matrix::identity());
+
+    // move x+1, then rotate cw
+    let m1 = Matrix::rotate90cw(Axis::Z).mul(&Matrix::translate(&Vector([1, 0, 0])));
+    // rotate cw, then move y-1
+    let m2 = Matrix::translate(&Vector([0, -1, 0])).mul(&Matrix::rotate90cw(Axis::Z));
+    assert_eq!(m1, m2);
 }
 
 fn test_all_orientations() {
